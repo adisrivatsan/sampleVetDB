cc2bc5c735eb270fd8ac834d6e6ff04d10de198a
Aditya Srivatsan <adityasrivatsan@Adityas-MacBook-Pro.local>
2016-06-06 19:26:23 -0400

    try

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
+    morgan = require('morgan'),
+    restful = require('node-restful'),
+    mongoose = restful.mongoose;
+var app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+app.use(methodOverride());
+
+mongoose.connect("mongodb://localhost/");
+
+var Resource = app.resource = restful.model('resource', mongoose.Schema({
+    title: String,
+    year: Number,
+  }))
+  .methods(['get', 'post', 'put', 'delete']);
+
+Resource.register(app, '/resources');
+
+app.listen(3000);
+console.log('listening on port 3000');
