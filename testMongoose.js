bbbe78a8e588827edf713e9e6756147f675266ab
Aditya Srivatsan <adityasrivatsan@Adityas-MacBook-Pro.local>
2016-06-15 23:55:30 -0400

    welcome screen

--- testMongoose.js
+++ testMongoose.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://adisri:srivatsan21@ds015334.mlab.com:15334/heroku_8bp865gl');
+mongoose.connect('mongodb://adisri:srivatsan21@ds015934.mlab.com:15934/heroku_hmbhcsh6');
 
 var VendorSchema = mongoose.Schema({
     Name: String,
@@ -10,28 +10,15 @@ var Vendor = mongoose.model('Vendor', VendorSchema);
 
 var Terry = new Vendor({ Name:'Terry', Hour: '10-11' });
 
-var saveFunction = function() {
-  Terry.save(function (err, ter) {
-    if (err) return console.error(err);
-    console.log('working');
-  });
-}
-
-
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback () {
-  var data = db.collection('FoodTruckVendorInfo').find();
+  //var data = db.collection('FoodTruckVendorInfo').find();
   //console.log(data);
-  Vendor.find(function (err, vendors) {
-  //console.log('hello' + vendors);
-  for(x in vendors) {
-    console.log(vendors[x].Hour);
-  }
-  db.close();
-  })
-
+  console.log('hello');
+  Vendor.find(function (err, ven) {
+  if (err) return console.error(err);
+  console.log(ven);
+})
 
 });
-
-//db.close();
